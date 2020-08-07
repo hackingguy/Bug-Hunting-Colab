fb8578095404f2c94b239483f6454cc4b169a933
Akash Chhabra <akashchhabra710@gmail.com>
2020-08-07 20:51:31 +0530

    Update ttmg.py

--- ttmg.py
+++ ttmg.py
@@ -404,7 +404,7 @@ def updateCheck(self, Version):
           return message
 
     check = UpdateChecker()
-    check.URL = "https://raw.githubusercontent.com/biplobsd/Google-Colab-CloudTorrent/master/update.xml"
+    check.URL = "https://raw.githubusercontent.com/hackingguy/Bug-Hunting-Colab/master/update.xml"
     currentVersion = check.getVersion("currentVersion")
     message = check.getMessage("message")
 
